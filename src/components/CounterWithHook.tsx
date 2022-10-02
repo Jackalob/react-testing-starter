1656ade1252598700eca3751204856169452a123
Jackalob <nike9712@gmail.com>
2022-10-02 23:27:09 +0800

    change function name in CounterWithHook

--- src/components/CounterWithHook.tsx
+++ src/components/CounterWithHook.tsx
@@ -6,9 +6,9 @@ type IProps = {
   step?: number;
 };
 
-const Counter = ({ value = 0, step = 1 }: IProps = {}) => {
+const CounterWithHook = ({ value = 0, step = 1 }: IProps = {}) => {
   const { count, increment } = useCounter({ value, step });
   return <button onClick={increment}>Count is {count}</button>;
 };
 
-export default Counter;
+export default CounterWithHook;
